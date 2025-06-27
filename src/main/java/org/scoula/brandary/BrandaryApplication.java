c42fa414a186fb7672c7bcf606dd6e9c3c5cc73c
tjdgus3488 <tjdgus3488@naver.com>
2025-06-27 16:07:07 +0900

    feat: first commit

---
+++ src/main/java/org/scoula/brandary/BrandaryApplication.java
@@ -0,0 +1,13 @@
+package org.scoula.brandary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/scoula/brandary/BrandaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scoula.brandary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
